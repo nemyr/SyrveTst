e56f189b6a435bfa35c0850fc00c6ab5ab30b2fb
nemyr <misha.zhban@gmail.com>
2023-09-25 20:22:20 +0300

    second attempt on another data set

--- SyrveTst/Program.cs
+++ SyrveTst/Program.cs
@@ -1,26 +1,26 @@
 ﻿int[][] tstArray = new int[][] {
-    Array.Empty<int>() , //0
-    new int[]{6},       //1
-    new int[]{1,7,8},   //2
-    new int[]{2, 1},    //3
-    new int[]{2, 1},    //4
-    new int[]{3, 1},    //5
-    new int[]{0},       //6
-    new int[]{5},       //7
-    new int[]{1, 3}     //8
+    Array.Empty<int>(), //0
+    new int[]{0},       //1
+    new int[]{0, 4},    //2
+    new int[]{},        //3
+    new int[]{3},       //4
+    new int[]{3},       //5
+    new int[]{3},       //6
+  //  new int[]{5},       //7
+   // new int[]{1, 3}     //8
 };
 
 
 bool CheckList(int[][] source, out int[] nodes ) {
 
-    HashSet<int> visitedNodes = new();
+    List<int> visitedNodes = new();
 
     bool isChanged;
     do {
         isChanged = false;
         for (int i = 0; i < source.Length; i++)
         {
-            if ((source[i].Length == 0 || visitedNodes.Any(e => source[i].Contains(e))) && !visitedNodes.Contains(i))
+            if ((source[i].Length == 0 || source[i].All(visitedNodes.Contains)) && !visitedNodes.Contains(i))
             {
                 visitedNodes.Add(i);
                 isChanged = true;
